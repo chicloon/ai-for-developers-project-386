69945171ea613bd76161f3ecc6a4220a749e742a
chicloon <chicloon@gmail.com>
2026-04-08 00:02:22 +0300

    fix: resolve struct naming conflicts, remove old models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,76 +1,9 @@
 package models
 
-import (
-	"time"
-
-	"github.com/jackc/pgx/v5/pgtype"
-)
-
-type TimeRange struct {
-	StartTime string `json:"startTime"`
-	EndTime   string `json:"endTime"`
-}
-
-type AvailabilityRule struct {
-	ID         string      `json:"id"`
-	Type       string      `json:"type"`
-	DayOfWeek  *int32      `json:"dayOfWeek,omitempty"`
-	Date       *string     `json:"date,omitempty"`
-	TimeRanges []TimeRange `json:"timeRanges"`
-}
-
-type CreateAvailabilityRule struct {
-	Type       string      `json:"type"`
-	DayOfWeek  *int32      `json:"dayOfWeek,omitempty"`
-	Date       *string     `json:"date,omitempty"`
-	TimeRanges []TimeRange `json:"timeRanges"`
-}
-
-type BlockedDay struct {
-	ID   string `json:"id"`
-	Date string `json:"date"`
-}
-
-type CreateBlockedDay struct {
-	Date string `json:"date"`
-}
-
-type Slot struct {
-	ID        string `json:"id"`
-	Date      string `json:"date"`
-	StartTime string `json:"startTime"`
-	EndTime   string `json:"endTime"`
-	IsBooked  bool   `json:"isBooked"`
-}
-
-type Booking struct {
-	ID            string  `json:"id"`
-	SlotDate      string  `json:"slotDate"`
-	SlotStartTime string  `json:"slotStartTime"`
-	Name          string  `json:"name"`
-	Email         string  `json:"email"`
-	Status        string  `json:"status"`
-	Recurrence    *string `json:"recurrence,omitempty"`
-	DayOfWeek     *int32  `json:"dayOfWeek,omitempty"`
-	EndDate       *string `json:"endDate,omitempty"`
-}
-
-type CreateBooking struct {
-	SlotDate      string  `json:"slotDate"`
-	SlotStartTime string  `json:"slotStartTime"`
-	Name          string  `json:"name"`
-	Email         string  `json:"email"`
-	Recurrence    *string `json:"recurrence,omitempty"`
-	DayOfWeek     *int32  `json:"dayOfWeek,omitempty"`
-	EndDate       *string `json:"endDate,omitempty"`
-}
-
+// ErrorResponse represents an error response
 type ErrorResponse struct {
 	Error string `json:"error"`
 }
-
-var _ = time.Time{}
-var _ = pgtype.Text{}
 
 // User represents a registered user
 type User struct {
@@ -151,7 +84,7 @@ type GroupMember struct {
 }
 
 // Booking with user info
-type BookingWithUser struct {
+type Booking struct {
 	ID          string  `json:"id"`
 	ScheduleID  string  `json:"scheduleId"`
 	Booker      User    `json:"booker"`
@@ -170,7 +103,7 @@ type CreateBookingRequest struct {
 }
 
 // Slot for public display
-type PublicSlot struct {
+type Slot struct {
 	ID        string `json:"id"`
 	Date      string `json:"date"`
 	StartTime string `json:"startTime"`
